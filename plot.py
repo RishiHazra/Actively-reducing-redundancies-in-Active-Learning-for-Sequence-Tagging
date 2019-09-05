89b2a8e8ede6983198fdc740a0a3ee9f62b690fe
RishiHazra <36856187+RishiHazra@users.noreply.github.com>
2019-09-06 00:05:35 +0530

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 29 14:21:55 2018
-@author: rishi
-CNN BiLSTM CRF
-"""
+
 import matplotlib.pyplot as plt
 from statistics import mean
 # t = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
